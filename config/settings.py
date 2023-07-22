815457a56c2036bea0d28ad8e5f163f82d0e4756
hdydtdxdt <140029239+hdydtdxdt@users.noreply.github.com>
2023-07-22 16:42:09 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -119,10 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    STATIC_DIR,
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, '/web_static/')
+STATIC_ROOT = os.path.join(BASE_DIR, '.static_root')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
